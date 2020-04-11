be907be4562ccd2f107e42dfb68d79b9901efb83
Andrii <Luckyhidalgo@gmail.com>
2020-04-11 13:26:26 +0300

    Created footer component.

---
+++ dest/js/app.js
@@ -0,0 +1,58 @@
+"use strict";
+
+/**
+ * @name initPreventBehavior
+ *
+ * @description
+ */
+var initPreventBehavior = function initPreventBehavior() {
+
+	var link = document.querySelectorAll("a");
+
+	link.forEach(function (val, idx) {
+
+		val.addEventListener("click", function (e) {
+			if (val.getAttribute("href") === "#") {
+				e.preventDefault();
+			}
+		});
+	});
+};
+
+/**
+ * @description Document DOM ready.
+ */
+(function () {
+	/*
+ * =============================================
+ * CALLBACK :: start
+ * ============================================= */
+	var footerBtnToggle = function footerBtnToggle() {
+		$('[footer-toggle-js]').on('click', function (ev) {
+			// $('[footer-toggle-js]').removeClass('is-active');
+			// $(ev.currentTarget).addClass('is-active');
+		});
+	};
+	/*
+ * CALLBACK :: end
+ * ============================================= */
+
+	/**
+  * @name initNative
+  *
+  * @description Init all method
+  */
+	var initNative = function initNative() {
+		// default
+		initPreventBehavior();
+		// ==========================================
+
+		// lib
+		// ==========================================
+
+		// callback
+		footerBtnToggle();
+		// ==========================================
+	};
+	initNative();
+})();
--- src/js/_document/indexNative.js
+++ src/js/_document/indexNative.js
@@ -6,7 +6,12 @@
 	* =============================================
 	* CALLBACK :: start
 	* ============================================= */
-
+	const footerBtnToggle = () => {
+		$('[footer-toggle-js]').on('click', (ev) => {
+			// $('[footer-toggle-js]').removeClass('is-active');
+			// $(ev.currentTarget).addClass('is-active');
+		});
+	};
 	/*
 	* CALLBACK :: end
 	* ============================================= */
@@ -26,6 +31,7 @@
 		// ==========================================
 
 		// callback
+		footerBtnToggle();
 		// ==========================================
 	};
 	initNative();
