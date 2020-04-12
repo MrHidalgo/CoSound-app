cce0154a3244215d85b2a40fbbee484208645b66
Andrii <Luckyhidalgo@gmail.com>
2020-04-12 14:30:34 +0300

    Card page - card swiper - filled card and card preview - w\o animation setting. smooth & adaptive.

--- dest/js/app.js
+++ dest/js/app.js
@@ -60,6 +60,7 @@ var initSwiper = function initSwiper() {
  * @description Document DOM ready.
  */
 (function () {
+	var waveSurferCard = undefined;
 	/*
  * =============================================
  * CALLBACK :: start
@@ -68,6 +69,105 @@ var initSwiper = function initSwiper() {
 		$('[footer-toggle-js]').on('click', function (ev) {
 			$('[footer-toggle-js]').removeClass('is-active');
 			$(ev.currentTarget).addClass('is-active');
+		});
+	};
+
+	var favoriteToggle = function favoriteToggle() {
+		$('[favorite-js]').on('click', function (ev) {
+			$(ev.currentTarget).toggleClass('is-active');
+		});
+	};
+
+	var cardOpen = function cardOpen() {
+		var formatTime = function formatTime(time) {
+			return [Math.floor(time % 3600 / 60), // minutes
+			('00' + Math.floor(time % 60)).slice(-2) // seconds
+			].join(':');
+		};
+
+		$('[card-btn-open-js]').on('click', function (ev) {
+			var _elParent = $(ev.currentTarget).closest('[card-js]');
+
+			$('body, html').addClass('is-card');
+
+			$('[card-preview-js]').append(_elParent.clone());
+
+			setTimeout(function () {
+				waveSurferCard = WaveSurfer.create({
+					container: document.querySelector('[card-preview-js] #cardwave'),
+					waveColor: '#5A5A5A',
+					progressColor: '#fff',
+					cursorColor: '#fff',
+					barWidth: 3,
+					barRadius: 3,
+					cursorWidth: 0,
+					height: 110,
+					barGap: 3,
+					responsive: true
+				});
+
+				waveSurferCard.load('media/audio.mp3');
+
+				waveSurferCard.on('audioprocess', function () {
+					$('.card__box-audio-currentTime').text(formatTime(waveSurferCard.getDuration() - waveSurferCard.getCurrentTime()));
+				});
+
+				waveSurferCard.on('ready', function () {
+					$('.card__box-audio-currentTime').text(formatTime(waveSurferCard.getDuration()));
+					$('.card__box-audio-duration').text(formatTime(waveSurferCard.getDuration()));
+				});
+			}, 0);
+
+			cardPlayToggle();
+		});
+	};
+
+	var cardBack = function cardBack() {
+		$('[card-back-js]').on('click', function (ev) {
+			$('body, html').removeClass('is-card');
+
+			$('[card-preview-js]').find('[card-js]').remove();
+
+			waveSurferCard.empty();
+			waveSurferCard.destroy();
+		});
+	};
+
+	var cardVolume = function cardVolume() {
+		var _count = 0;
+
+		$(document).on('click', '[card-preview-js] [card-volume-js]', function (ev) {
+			var _el = $(ev.currentTarget),
+			    _countNode = $('[card-volume-count-js]');
+
+			if (_count > 3 || _count < 0) {
+				return false;
+			}
+
+			_count += 1;
+
+			if (_count === 1) {
+				_countNode.text('+ 1');
+				_el.addClass('is-volume-one');
+			} else if (_count === 2) {
+				_countNode.text('+ 2');
+				_el.removeClass('is-volume-one').addClass('is-volume-two');
+			} else if (_count === 3) {
+				_countNode.text('+ 3');
+				_el.removeClass('is-volume-one, is-volume-two').addClass('is-volume-three');
+			}
+		});
+	};
+
+	var cardPlayToggle = function cardPlayToggle() {
+		$('[card-play-js]').on('click', function (ev) {
+			if ($(ev.currentTarget).hasClass('is-play')) {
+				$(ev.currentTarget).removeClass('is-play');
+				waveSurferCard.pause();
+			} else {
+				$(ev.currentTarget).addClass('is-play');
+				waveSurferCard.play();
+			}
 		});
 	};
 	/*
@@ -89,8 +189,11 @@ var initSwiper = function initSwiper() {
 		// ==========================================
 
 		// callback
-		// fullViewHeight();
 		footerBtnToggle();
+		favoriteToggle();
+		cardOpen();
+		cardBack();
+		cardVolume();
 		// ==========================================
 	};
 	initNative();
--- src/js/_document/indexNative.js
+++ src/js/_document/indexNative.js
@@ -2,6 +2,7 @@
  * @description Document DOM ready.
  */
 (function () {
+	let waveSurferCard = undefined;
 	/*
 	* =============================================
 	* CALLBACK :: start
@@ -10,6 +11,111 @@
 		$('[footer-toggle-js]').on('click', (ev) => {
 			$('[footer-toggle-js]').removeClass('is-active');
 			$(ev.currentTarget).addClass('is-active');
+		});
+	};
+
+
+	const favoriteToggle = () => {
+		$('[favorite-js]').on('click', (ev) => {
+			$(ev.currentTarget).toggleClass('is-active');
+		});
+	};
+
+
+	const cardOpen = () => {
+		const formatTime = function (time) {
+			return [
+				Math.floor((time % 3600) / 60), // minutes
+				('00' + Math.floor(time % 60)).slice(-2) // seconds
+			].join(':');
+		};
+
+		$('[card-btn-open-js]').on('click', (ev) => {
+			const _elParent = $(ev.currentTarget).closest('[card-js]');
+
+			$('body, html').addClass('is-card');
+
+			$('[card-preview-js]').append(_elParent.clone());
+
+			setTimeout(() => {
+				waveSurferCard = WaveSurfer.create({
+					container: document.querySelector('[card-preview-js] #cardwave'),
+					waveColor: '#5A5A5A',
+					progressColor: '#fff',
+					cursorColor: '#fff',
+					barWidth: 3,
+					barRadius: 3,
+					cursorWidth: 0,
+					height: 110,
+					barGap: 3,
+					responsive: true
+				});
+
+				waveSurferCard.load('media/audio.mp3');
+
+				waveSurferCard.on('audioprocess', function () {
+					$('.card__box-audio-currentTime').text( formatTime(waveSurferCard.getDuration() - waveSurferCard.getCurrentTime()) );
+				});
+
+				waveSurferCard.on('ready', function () {
+					$('.card__box-audio-currentTime').text( formatTime(waveSurferCard.getDuration()) );
+					$('.card__box-audio-duration').text( formatTime(waveSurferCard.getDuration()) );
+				});
+			}, 0);
+
+			cardPlayToggle();
+		});
+	};
+
+
+	const cardBack = () => {
+		$('[card-back-js]').on('click', (ev) => {
+			$('body, html').removeClass('is-card');
+
+			$('[card-preview-js]').find('[card-js]').remove();
+
+			waveSurferCard.empty();
+			waveSurferCard.destroy();
+		});
+	};
+
+
+	const cardVolume = () => {
+		let _count = 0;
+
+		$(document).on('click', '[card-preview-js] [card-volume-js]', (ev) => {
+			const _el = $(ev.currentTarget),
+				_countNode = $('[card-volume-count-js]');
+
+			if (_count > 3 || _count < 0) {
+				return false;
+			}
+
+			_count += 1;
+
+			if(_count === 1) {
+				_countNode.text('+ 1');
+				_el.addClass('is-volume-one');
+			} else if (_count === 2) {
+				_countNode.text('+ 2');
+				_el.removeClass('is-volume-one').addClass('is-volume-two');
+			} else if (_count === 3) {
+				_countNode.text('+ 3');
+				_el.removeClass('is-volume-one, is-volume-two').addClass('is-volume-three');
+			}
+		});
+	};
+
+
+	const cardPlayToggle = () => {
+		$('[card-play-js]').on('click', (ev) => {
+			if($(ev.currentTarget).hasClass('is-play')) {
+				$(ev.currentTarget).removeClass('is-play');
+				waveSurferCard.pause();
+			} else {
+				$(ev.currentTarget).addClass('is-play');
+				waveSurferCard.play();
+			}
 		});
 	};
 	/*
@@ -32,8 +138,11 @@
 		// ==========================================
 
 		// callback
-		// fullViewHeight();
 		footerBtnToggle();
+		favoriteToggle();
+		cardOpen();
+		cardBack();
+		cardVolume();
 		// ==========================================
 	};
 	initNative();
